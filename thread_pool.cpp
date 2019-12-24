d5f7fa85e0a5d33e03fec443aa162cb2c7c03441
Damon Revoe <him@revl.org>
2019-12-24 13:34:53 -0500

    Initial adaptation of 2006 edition for c++11

---
+++ thread_pool.cpp
@@ -0,0 +1,273 @@
+#include "thread_pool.hpp"
+
+#include <thread>
+#include <cassert>
+
+// Element of the linked lists in the thread pool object.
+struct thread_pool::thread_wrapper final : std::thread {
+    // Starts a new worker thread.
+    thread_wrapper(thread_pool* p) :
+        std::thread(&thread_wrapper::thread_proc, this),
+        pool(p),
+        signaled(false)
+    {
+        signaling_mutex.lock();
+
+        add_to(&p->active_threads);
+
+        ++p->current_thread_count;
+    }
+
+    // Wakes this thread up.
+    void signal()
+    {
+        do {
+            signaling_mutex.unlock();
+            signaling_mutex.lock();
+        } while (!signaled);
+
+        signaled = false;
+    }
+
+    // Waits for this thread to finish, removes it from the containing
+    // list, and deletes this object.
+    void join_and_delete()
+    {
+        pool->global_mutex.unlock();
+
+        join();
+
+        pool->global_mutex.lock();
+
+        unlink();
+
+        delete this;
+    }
+
+    // Joins all threads that are in the list of finished threads.
+    static void join_all_finished(thread_pool* p)
+    {
+        while (p->finished_threads != nullptr)
+            p->finished_threads->join_and_delete();
+    }
+
+    // Suspends this thread until another thread calls the signal() method.
+    void wait_for_signal()
+    {
+        std::lock_guard<std::mutex> lock(signaling_mutex);
+        signaled = true;
+    }
+
+    // Detaches this thread from the containing list.
+    void unlink()
+    {
+        if (next != nullptr)
+            next->prev = prev;
+
+        if (prev != nullptr)
+            prev->next = next;
+        else
+            *current_list_head = next;
+    }
+
+    // Inserts this thread into the specified list.
+    void add_to(thread_wrapper** list_head)
+    {
+        if (*list_head != nullptr) {
+            (next = *list_head)->prev = this;
+            prev = nullptr;
+        } else
+            next = prev = nullptr;
+
+        *(current_list_head = list_head) = this;
+    }
+
+    // Moves this thread from its current list to another list.
+    void move_to(thread_wrapper** other_list_head)
+    {
+        if (current_list_head != other_list_head) {
+            unlink();
+            add_to(other_list_head);
+        }
+    }
+
+    // The worker thread routine.
+    void thread_proc();
+
+    // The thread pool object that this thread belongs to.
+    thread_pool* pool;
+
+    // The list in the pool object that contains this thread.
+    thread_wrapper** current_list_head;
+
+    // The previous element in the list that contains this thread.
+    thread_wrapper* prev;
+
+    // The next element in the list that contains this thread.
+    thread_wrapper* next;
+
+    // Mutex for suspending and resuming this thread.
+    std::mutex signaling_mutex;
+
+    // Signal delivery confirmation flag. It helps avoid the race condition
+    // when the master thread unlocks and then immediately locks the signaling
+    // mutex after the worker thread has registered itself as available, but
+    // before it acquired a lock on the signaling mutex.
+    bool signaled;
+};
+
+void thread_pool::thread_wrapper::thread_proc()
+{
+    pool->global_mutex.lock();
+
+    do {
+        if (pool->task_queue.empty()) {
+            move_to(&pool->suspended_threads);
+
+            pool->global_mutex.unlock();
+
+            wait_for_signal();
+        } else {
+            move_to(&pool->active_threads);
+
+            std::function<void()> task = std::move(pool->task_queue.front());
+            pool->task_queue.pop_front();
+
+            pool->global_mutex.unlock();
+
+            // The packaged_task class takes care of the potential exceptions.
+            task();
+        }
+
+        pool->global_mutex.lock();
+
+        join_all_finished(pool);
+    } while (pool->current_thread_count <= pool->max_thread_count);
+
+    move_to(&pool->finished_threads);
+
+    --pool->current_thread_count;
+
+    pool->global_mutex.unlock();
+}
+
+thread_pool::thread_pool() :
+    active_threads(nullptr),
+    suspended_threads(nullptr),
+    finished_threads(nullptr),
+    current_thread_count(0),
+    max_thread_count(0)
+{}
+
+thread_pool::thread_pool(int min_threads, int max_threads) : thread_pool()
+{
+    resize(min_threads, max_threads);
+}
+
+void thread_pool::resize(int min_threads, int max_threads)
+{
+    assert(min_threads <= max_threads);
+
+    std::lock_guard<std::mutex> lock(global_mutex);
+
+    max_thread_count = max_threads;
+
+    // Check if the number of currently running threads exceeds the requested
+    // maximum.
+    int delta = current_thread_count - max_threads;
+    if (delta > 0) {
+        // Wake up suspended threads to reduce the number of threads. When a
+        // worker thread wakes up, it checks whether the number of running
+        // threads exceeds the maximum, and if that is the case, the thread
+        // terminates.
+        for (thread_wrapper* t = suspended_threads; t != nullptr; t = t->next) {
+            t->signal();
+
+            if (--delta == 0)
+                break;
+        }
+    } else {
+        // Check if the number of running threads is fewer than 'min_threads'
+        // or if more threads can be started to accommodate the remaining
+        // tasks in the queue.
+        delta = current_thread_count + static_cast<int>(task_queue.size());
+
+        if (delta > max_threads)
+            delta = max_threads;
+        else if (delta < min_threads)
+            delta = min_threads;
+
+        delta -= current_thread_count;
+
+        while (--delta >= 0)
+            new thread_wrapper(this);
+    }
+
+    thread_wrapper::join_all_finished(this);
+}
+
+void thread_pool::shutdown()
+{
+    std::lock_guard<std::mutex> lock(global_mutex);
+
+    max_thread_count = 0;
+
+    while (suspended_threads != nullptr) {
+        suspended_threads->signal();
+
+        suspended_threads->join_and_delete();
+    }
+
+    while (active_threads != nullptr)
+        active_threads->join_and_delete();
+
+    thread_wrapper::join_all_finished(this);
+}
+
+thread_pool::~thread_pool()
+{
+    try {
+        shutdown();
+    }
+    catch (...) {
+    }
+}
+
+void thread_pool::add_task_and_wake_up_thread(std::function<void()>&& new_task)
+{
+    std::lock_guard<std::mutex> lock(global_mutex);
+
+    task_queue.emplace_back(std::move(new_task));
+
+    if (suspended_threads != nullptr)
+        suspended_threads->signal();
+    else if (current_thread_count < max_thread_count)
+        new thread_wrapper(this);
+}
+
+thread_pool::thread_pool(thread_pool&& other) noexcept : thread_pool()
+{
+    std::lock_guard<std::mutex> lock(other.global_mutex);
+
+    task_queue = std::move(other.task_queue);
+
+    auto move_thread_list = [this](thread_wrapper** to, thread_wrapper** from) {
+        *to = *from;
+        *from = nullptr;
+
+        for (thread_wrapper* t = *from; t != nullptr; t = t->next) {
+            t->pool = this;
+            t->current_list_head = to;
+        }
+    };
+
+    move_thread_list(&active_threads, &other.active_threads);
+    move_thread_list(&suspended_threads, &other.suspended_threads);
+    move_thread_list(&finished_threads, &other.finished_threads);
+
+    current_thread_count = other.current_thread_count;
+    other.current_thread_count = 0;
+
+    max_thread_count = other.max_thread_count;
+    other.max_thread_count = 0;
+}
---
+++ thread_pool.hpp
@@ -0,0 +1,124 @@
+#ifndef THREAD_POOL_ON_MUTEXES_H
+#define THREAD_POOL_ON_MUTEXES_H
+
+#include <future>
+#include <memory>
+#include <deque>
+#include <functional>
+#include <mutex>
+
+// A thread pool implementation that uses only std::mutex for synchronization
+// and signaling.
+//
+// Usage example:
+//  thread_pool tp(4, 8);
+//  auto future_blob = tp.enqueue([rc] {return rc->read_blob();});
+//  auto blob = future_blob.get();
+//  tp.shutdown();
+//
+// This class is thread-safe; any public method can be called from any thread
+// in the current process, including the worker threads of the thread pool
+// itself.
+class thread_pool final
+{
+public:
+    // Initializes this thread pool instance but does not start any threads.
+    // No threads will be started until resize() is called with a non-zero
+    // 'max_threads' value.
+    thread_pool();
+
+    // Initializes this thread pool instance and starts the specified minimum
+    // number of worker threads.
+    thread_pool(int min_threads, int max_threads);
+
+    // Enqueues a task, which must be a callable object that has no arguments.
+    //
+    // If there is a vacant thread, it will pick up the task immediately.
+    // Otherwise, this method will create a new thread to run the task unless
+    // the pool is at the maximum allowed capacity, in which case the task will
+    // remain in the queue until an existing thread becomes available.
+    //
+    // The method returns a future, which will receive the return value of the
+    // task.
+    template <typename F>
+    auto enqueue(F&& f) -> std::future<decltype(f())>
+    {
+        using PT = std::packaged_task<decltype(f())()>;
+        auto pt = std::make_shared<PT>(std::move(f));
+        add_task_and_wake_up_thread(std::bind(&PT::operator(), pt));
+        return pt->get_future();
+    }
+
+    // Adjusts the number of worker threads so that it falls within the range
+    // defined by the 'min_threads' and 'max_threads' parameters.
+    //
+    // If the number of currently running tasks in the pool already exceeds
+    // 'max_threads', this method returns without directly affecting the
+    // threads that run those tasks. Instead, the threads will terminate
+    // gradually as their tasks complete until only 'max_threads' are running.
+    void resize(int min_threads, int max_threads);
+
+    // Returns the number of currently running threads.
+    int thread_count() const;
+
+    // Returns the size of the task queue.
+    int waiting_tasks() const;
+
+    // Waits for completion of all running tasks and terminates all worker
+    // threads. To start the threads again, call resize().
+    void shutdown();
+
+    // Calls shutdown().
+    ~thread_pool();
+
+    // Allows for moving thread pool instances. Note that in the unlikely
+    // event that 'other.global_mutex.lock()' throws an exception, the
+    // program will terminate.
+    thread_pool(thread_pool&& other) noexcept;
+
+private:
+    void add_task_and_wake_up_thread(std::function<void()>&& new_task);
+
+    // The queue of tasks to be processed.
+    std::deque<std::function<void()>> task_queue;
+
+    struct thread_wrapper;
+    friend struct thread_wrapper;
+
+    // The first element in the linked list of threads that are currently
+    // processing tasks.
+    thread_wrapper* active_threads;
+
+    // The first element in the linked list of threads that are dormant
+    // (because the task queue is empty).
+    thread_wrapper* suspended_threads;
+
+    // The first element in the linked list of threads that have exited.
+    thread_wrapper* finished_threads;
+
+    // The number of currently running threads (the total number of threads
+    // in the 'active_threads' and 'suspended_threads' lists combined).
+    int current_thread_count;
+
+    // The maximum number of threads that this pool is allowed to have.
+    int max_thread_count;
+
+    // The mutex that provides exclusive access to the data members of this
+    // thread pool.
+    std::mutex global_mutex;
+
+    thread_pool(const thread_pool&) = delete;
+    thread_pool& operator=(const thread_pool&) = delete;
+};
+
+inline int thread_pool::thread_count() const
+{
+    return current_thread_count;
+}
+
+inline int thread_pool::waiting_tasks() const
+{
+    return static_cast<int>(task_queue.size());
+}
+
+#endif // THREAD_POOL_ON_MUTEXES_H
